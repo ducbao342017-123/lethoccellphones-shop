fc871b736a360173597c0c0a0dfbb7f88b3f6066
Le Giang <legiang262@gmail.com>
2026-07-27 11:26:52 +0700

    add_batch_helper

---
+++ push_to_github.bat
@@ -0,0 +1,13 @@
+@echo off
+title DANG DAY CODE LEN GITHUB - LETHOCCELLPHONE
+cd /d "%~dp0"
+echo =======================================================
+echo     DANG DAY CODE LEN GITHUB CHO DUCBAO342017-123
+echo =======================================================
+echo.
+git push -u origin master --force
+echo.
+echo =======================================================
+echo     DA DAY CODE XONG! AN PHIM BAT KY DE THOAT.
+echo =======================================================
+pause
